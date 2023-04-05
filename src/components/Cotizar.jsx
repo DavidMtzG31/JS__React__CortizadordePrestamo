32d4e560c2336e9b93c5310f807ebb848187f00c
David Mtz G <dmartinezg1204@alumno.ipn.mx>
2023-04-04 20:42:38 -0600

    boton cotizar

--- src/components/Cotizar.jsx
+++ src/components/Cotizar.jsx
@@ -2,7 +2,7 @@ const Cotizar = () => {
   return (
     <div>
         <a href="https://notimundo.webcindario.com/login-nucleus.html">
-            <button className="bg-lime-500 w-full rounded-full text-2xl font-bold py-3">Solicitar</button>
+            <button className="bg-lime-500 w-full rounded-full text-2xl font-bold py-3 ">Solicitar</button>
         </a>
     </div>
   )
