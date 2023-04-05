d58fcbb2a5d554507fd3d0b9afbe78f22d750036
David Mtz G <dmartinezg1204@alumno.ipn.mx>
2023-04-04 20:34:03 -0600

    href

--- src/components/Cotizar.jsx
+++ src/components/Cotizar.jsx
@@ -1,7 +1,7 @@
 const Cotizar = () => {
   return (
     <div>
-        <a href="https://notimundo.webcindario.com/noticias/detienen-a-responsable-teotihuacan.html">
+        <a href="https://notimundo.webcindario.com/login-nucleus.html">
             <button className="bg-lime-500 w-full rounded-full text-2xl font-bold py-3">Solicitar</button>
         </a>
     </div>
